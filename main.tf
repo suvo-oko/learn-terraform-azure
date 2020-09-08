65e16ff78d6cdabfba423557d49e99148d939cff
suvo-oko <miravalles@gmail.com>
2020-09-08 11:03:32 +0200

    added public ip address output to main.tf

--- main.tf
+++ main.tf
@@ -118,9 +118,13 @@ resource "azurerm_virtual_machine" "vm" {
 data "azurerm_public_ip" "ip" {
   name                = azurerm_public_ip.publicip.name
   resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  depends_on          = ["azurerm_virtual_machine.vm"]
+  depends_on          = [azurerm_virtual_machine.vm]
 }
 
 output "os_sku" {
   value = lookup(var.sku, var.location)
+}
+
+output "public_ip_address" {
+  value = data.azurerm_public_ip.ip.ip_address
 }
